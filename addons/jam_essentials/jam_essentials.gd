33328d5c5615e709b8c49cfa3620c07cc8b03508
Moraguma <g170603@dac.unicamp.br>
2023-09-08 23:35:27 -0300

    Adds autoload logic

--- addons/jam_essentials/jam_essentials.gd
+++ addons/jam_essentials/jam_essentials.gd
@@ -2,11 +2,14 @@
 extends EditorPlugin
 
 
+const AUTOLOADS = {"SceneManager": "res://addons/scenes/SceneManager.tscn"}
+
+
 func _enter_tree():
-	# Initialization of the plugin goes here.
-	pass
+	for autoload_name in AUTOLOADS:
+		add_autoload_singleton(autoload_name, AUTOLOADS[autoload_name])
 
 
 func _exit_tree():
-	# Clean-up of the plugin goes here.
-	pass
+	for autoload_name in AUTOLOADS:
+		remove_autoload_singleton(autoload_name)
